283e2adbc66e1df1b294250a4001cc8462a78cbb
neonvoidx <me@neonvoid.dev>
2025-12-10 09:27:26 -0500

    up

--- home/dots/bat.nix
+++ home/dots/bat.nix
@@ -1,8 +1,9 @@
 { lib, pkgs, ... }: {
   programs.bat = {
     enable = true;
+    config = { theme = "Eldritch"; };
     themes = {
-      src = pkgs.fetchFromGithub {
+      src = pkgs.fetchFromGitHub {
         owner = "eldritch-theme";
         repo = "bat";
         rev = "33f8a2543f626637e8d85356e85bf32eee414f17";
