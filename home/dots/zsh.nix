8184101b4c27d1cd66b944b8a23b7941814edb57
neonvoidx <me@neonvoid.dev>
2025-12-10 10:53:24 -0500

    up

--- home/dots/zsh.nix
+++ home/dots/zsh.nix
@@ -40,11 +40,11 @@
         "sindresorhus/pure"
         "Aloxaf/fzf-tab"
         "trystan2k/zsh-tab-title"
-        "zdharma-continuum/fast-syntax-highlighting"
         "ohmyzsh/ohmyzsh path:plugins/colored-man-pages"
         "ohmyzsh/ohmyzsh path:plugins/fancy-ctrl-z"
         "zsh-users/zsh-completions"
-        "jeffreytse/zsh-vi-mode"
+        "jeffreytse/zsh-vi-mode kind:defer"
+        "zdharma-continuum/fast-syntax-highlighting kind:defer"
       ];
     };
 
