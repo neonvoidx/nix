e178a75c6226138ff912226b10cf804860aaa8f3
neonvoidx <me@neonvoid.dev>
2025-12-10 14:48:03 -0500

    up

--- home/dots/nixvim/plugins/kitty.nix
+++ home/dots/nixvim/plugins/kitty.nix
@@ -1,39 +1,41 @@
 { pkgs, ... }: {
-  # Kitty terminal integration
+  programs.nixvim = {
 
-  # Kitty plugins via extraPlugins
-  extraPlugins = with pkgs.vimPlugins; [
-    # Kitty config syntax
-    vim-kitty
-    # Kitty navigator
-    {
-      plugin = pkgs.vimUtils.buildVimPlugin {
-        name = "vim-kitty-navigator";
-        src = pkgs.fetchFromGitHub {
-          owner = "knubie";
-          repo = "vim-kitty-navigator";
-          rev = "master";
-          sha256 = ""; # Will need to be filled
+    # Kitty plugins via extraPlugins
+    extraPlugins = with pkgs.vimPlugins; [
+      # Kitty config syntax
+      vim-kitty
+      # Kitty navigator
+      {
+        plugin = pkgs.vimUtils.buildVimPlugin {
+          name = "vim-kitty-navigator";
+          src = pkgs.fetchFromGitHub {
+            owner = "knubie";
+            repo = "vim-kitty-navigator";
+            rev = "master";
+            sha256 = ""; # Will need to be filled
+          };
         };
-      };
-    }
-    # Kitty scrollback
-    {
-      plugin = pkgs.vimUtils.buildVimPlugin {
-        name = "kitty-scrollback.nvim";
-        src = pkgs.fetchFromGitHub {
-          owner = "mikesmithgh";
-          repo = "kitty-scrollback.nvim";
-          rev = "main";
-          sha256 = ""; # Will need to be filled
+      }
+      # Kitty scrollback
+      {
+        plugin = pkgs.vimUtils.buildVimPlugin {
+          name = "kitty-scrollback.nvim";
+          src = pkgs.fetchFromGitHub {
+            owner = "mikesmithgh";
+            repo = "kitty-scrollback.nvim";
+            rev = "main";
+            sha256 = ""; # Will need to be filled
+          };
         };
-      };
-    }
-  ];
+      }
+    ];
 
-  # Kitty configuration is handled in keymaps.nix for the navigator
-  extraConfigLua = ''
-    -- Kitty scrollback configuration
-    require('kitty-scrollback').setup()
-  '';
+    # Kitty configuration is handled in keymaps.nix for the navigator
+    extraConfigLua = # lua
+      ''
+        -- Kitty scrollback configuration
+        require('kitty-scrollback').setup()
+      '';
+  };
 }
