a7a174425f17138adfdfe2b8fdb3ca30cfcbbb6c
neonvoidx <me@neonvoid.dev>
2025-12-10 09:31:44 -0500

    up

--- home/common.nix
+++ home/common.nix
@@ -4,7 +4,15 @@
     inherit username;
     homeDirectory = "/home/${username}";
     stateVersion = "25.11";
+    sessionVariables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+      BROWSER = "firefox-developer-edition";
+      TERMINAL = "kitty";
+    };
   };
+
+  xdg = { enable = true; };
 
   home.packages = with pkgs; [ kitty proton-pass ];
 
--- home/dots/bat.nix
+++ home/dots/bat.nix
@@ -7,7 +7,7 @@
         owner = "eldritch-theme";
         repo = "bat";
         rev = "33f8a2543f626637e8d85356e85bf32eee414f17";
-        sha256 = "";
+        sha256 = lib.fakeSha256;
       };
       file = "Eldritch.tmTheme";
     };
