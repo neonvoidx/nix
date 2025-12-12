74c129be7b3491bd3aa8917dcd4b5c85298e0e89
neonvoidx <me@neonvoid.dev>
2025-12-11 21:31:52 -0500

    nixvim opts

--- home/dots/nixvim.nix
+++ home/dots/nixvim.nix
@@ -5,27 +5,76 @@
     enable = true;
     defaultEditor = true;
 
+    enableMan = true;
+    # colorscheme
+
     viAlias = true;
     vimAlias = true;
 
     opts = {
-      number = true;
-      relativenumber = true;
-      shiftwidth = 2;
-      tabstop = 2;
+      autowrite = true;
+      autoread = true;
+      clipboard = "unnamedplus";
+      completeopt = "menu,menuone,noselect";
+      conceallevel = 1;
+      confirm = true;
+      cursorline = true;
       expandtab = true;
-      smartindent = true;
-      wrap = false;
-      swapfile = false;
-      backup = false;
-      undofile = true;
-      hlsearch = false;
-      incsearch = true;
-      termguicolors = true;
-      scrolloff = 8;
+      formatoptions = "jcroqlnt";
+      grepformat = "%f:%l:%c:%m";
+      grepprg = "rg";
+      ignorecase = true;
+      inccommand = "nosplit";
+      laststatus = 3;
+      list = true;
+      mouse = "nv";
+      mousemoveevent = true;
+      number = true;
+      pumblend = 10;
+      pumheight = 10;
+      relativenumber = true;
+      scrolloff = 4;
+      sessionoptions = [
+        "buffers"
+        "curdir"
+        "tabpages"
+        "winsize"
+        "help"
+        "globals"
+        "skiprtp"
+        "folds"
+      ];
+      shiftround = true;
+      shiftwidth = 2;
+      showmode = false;
+      sidescrolloff = 8;
       signcolumn = "yes";
-      updatetime = 50;
-      colorcolumn = "80";
+      smartcase = true;
+      smartindent = true;
+      spelllang = [ "en" ];
+      splitbelow = true;
+      splitkeep = "screen";
+      splitright = true;
+      tabstop = 2;
+      termguicolors = true;
+      timeoutlen = 300;
+      undofile = true;
+      undolevels = 10000;
+      updatetime = 200;
+      virtualedit = "block";
+      wildmode = "longest:full,full";
+      winminwidth = 5;
+      wrap = false;
+      autochdir = false;
+      smoothscroll = true;
+      fillchars = {
+        foldopen = "";
+        foldclose = "";
+        fold = " ";
+        foldsep = " ";
+        diff = "╱";
+        eob = " ";
+      };
     };
   };
 }
