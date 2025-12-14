e39bad7b1ab5f0ae30522114cd8ae619ec8a3a5c
neonvoidx <me@neonvoid.dev>
2025-12-14 01:27:00 -0500

    lsd
    up

--- home/configs/common/default.nix
+++ home/configs/common/default.nix
@@ -1,13 +1,14 @@
 { ... }:
 {
   imports = [
-    ./nixcats
     ./bat.nix
     ./btop.nix
     ./cava.nix
     ./fastfetch.nix
     ./git.nix
     ./kitty.nix
+    ./lsd.nix
+    ./nixcats
     ./zsh.nix
   ];
 }
