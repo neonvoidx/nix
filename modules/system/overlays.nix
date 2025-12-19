9d0634e93a3869b75508f5fa642b2954305b24b1
neonvoidx <me@neonvoid.dev>
2025-12-19 15:21:48 -0500

    up

--- modules/system/overlays.nix
+++ modules/system/overlays.nix
@@ -1,7 +1,7 @@
 { pkgs, inputs, ... }:
 {
   nixpkgs.overlays = [
-    inputs.nix-cachyos-kernel.overlays.default
+    # inputs.nix-cachyos-kernel.overlays.default
     (final: prev: {
       nur = import inputs.nur {
         nurpkgs = prev;
@@ -11,5 +11,5 @@
   ];
 
   # CachyOS Kernel
-  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+  # boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
 }
