2d321183a3961559b09484e3c46dfd0595d9e5bb
neonvoidx <me@neonvoid.dev>
2025-12-19 14:20:33 -0500

    up

--- home/configs/common/nixvim/plugins/yazi.nix
+++ home/configs/common/nixvim/plugins/yazi.nix
@@ -18,4 +18,30 @@
       end
     '';
   };
+  keymaps = [
+    {
+      key = "<leader>e";
+      options = {
+        silent = true;
+        desc = "Yazi (current loc)";
+      };
+      mode = [
+        "n"
+        "v"
+      ];
+      action = "<cmd>Yazi<cr>";
+    }
+    {
+      key = "<leader>E";
+      options = {
+        silent = true;
+        desc = "Yazi (cwd)";
+      };
+      mode = [
+        "n"
+        "v"
+      ];
+      action = "<cmd>Yazi cwd<cr>";
+    }
+  ];
 }
