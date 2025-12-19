105e8ad47d71433f21ca115b577a27e91e9597c0
neonvoidx <me@neonvoid.dev>
2025-12-19 14:07:59 -0500

    up

--- home/configs/common/nixvim/plugins/format.nix
+++ home/configs/common/nixvim/plugins/format.nix
@@ -28,15 +28,6 @@
             return
           end
 
-          if slow_format_filetypes[vim.bo[bufnr].filetype] then
-            return
-          end
-
-          local function on_format(err)
-            if err and err:match("timeout$") then
-              slow_format_filetypes[vim.bo[bufnr].filetype] = true
-            end
-          end
 
           return { timeout_ms = 200, lsp_fallback = true }, on_format
          end
