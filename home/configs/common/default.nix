ecaebda09e57faed668acbf3b45ba42ba7cb980a
neonvoidx <me@neonvoid.dev>
2026-01-01 16:50:50 -0500

    Add direnv and nix-direnv, added global justfile added just global config, for creating quick dev shells

--- home/configs/common/default.nix
+++ home/configs/common/default.nix
@@ -3,9 +3,11 @@
   imports = [
     ./bat.nix
     ./btop.nix
+    ./direnv.nix
     ./fastfetch.nix
     ./fzf.nix
     ./git.nix
+    ./just.nix
     ./kitty.nix
     ./lsd.nix
     ./nixvim
---
+++ home/configs/common/just.nix
@@ -0,0 +1,6 @@
+{ config, ... }:
+{
+  home.file.".config/just/justfile" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/assets/justfile";
+  };
+}
