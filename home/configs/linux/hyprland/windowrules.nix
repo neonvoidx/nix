6949f6ce92eb46bfe3c02feb61c4580343eda9b8
neonvoidx <me@neonvoid.dev>
2026-01-01 14:34:51 -0500

    hyprland window rules rewrite

--- home/configs/linux/hyprland/windowrules.nix
+++ home/configs/linux/hyprland/windowrules.nix
@@ -9,50 +9,112 @@ let
 in
 {
   windowrule = [
-    "workspace 3 silent,class:^(vesktop)$"
-    "workspace 2 silent,title:^(Stream Deck UI)$"
-    "workspace 10 silent,class:^(steam|Steam)$"
-    "workspace 10 silent,title:^(Steamwebhelper)$"
-    "float,initialTitle:^(Sign in to Steam)$"
-    "float,class:^(SGDBoop)$"
-    "center,initialTitle:^(Sign in to Steam)$"
-    "pin,class:^(hyprland-dialog)$"
-    "suppressevent activatefocus,class:^(steam)$"
-    "workspace 11,class:^(gamescope)$"
+    {
+      name = "vesktop";
+      "match:class" = "vesktop";
+      workspace = "3 silent";
+    }
+    {
+      name = "steam";
+      "match:class" = "steam|Steam";
+      workspace = "10 silent";
+    }
+    {
+      name = "bnet";
+      "match:title" = "Battle.net";
+      float = "on";
+      workspace = "10";
+    }
+    {
+      name = "wow";
+      "match:title" = "^World of Warcraft$";
+      fullscreen = "on";
+      workspace = "11";
+    }
+    {
+      name = "thunderbirdreminder";
+      "match:class" = "org.mozilla.Thunderbird";
+      "match:title" = "^.*Reminder.*$";
+      suppress_event = "activatefocus";
+      float = "on";
+      pin = "on";
+      size = "(monitor_w*0.2) (monitor_h*0.3)";
+      move = "(monitor_w-(monitor_w*0.2)-10) (monitor_h-(monitor_h*0.3)-10)";
+    }
+    {
+      name = "kittydropdown";
+      "match:class" = "kittyquick";
+      float = "on";
+      pin = "on";
+    }
+    {
+      name = "pip";
+      "match:class" = "firefox";
+      "match:title" = "Picture-in-Picture";
+      suppress_event = "activatefocus";
+      no_initial_focus = "on";
+      float = "on";
+      pin = "on";
+      size = "(monitor_w*0.2) (monitor_h*0.3)";
+      move = "(monitor_w-(monitor_w*0.2)-10) (monitor_h-(monitor_h*0.3)-10)";
+    }
+    {
+      name = "steampopup";
+      "match:title" = "Steamwebhelper";
+      workspace = "10 silent";
+    }
+    {
+      name = "steamsignin";
+      "match:initial_title" = "Sign in to Steam";
+      center = true;
+      float = "on";
+    }
+    {
+      name = "sgdbooppopup";
+      "match:class" = "SGDBoop";
+      float = true;
+    }
+    {
+      name = "hyprpopup";
+      "match:class" = "hyprland-dialog";
+      pin = true;
+    }
+    {
+      name = "gamescopegames";
+      "match:class" = "gamescope";
+      workspace = "11";
+    }
   ]
   ++ lib.optionals isVoid [
-    "workspace 3 silent,class:^(spotify)$"
-    "workspace 4 silent,class:^(org.mozilla.Thunderbird)$"
-    "workspace 4 silent,class:^(org.gnome.Fractal)$"
+    {
+      name = "spotify";
+      "match:class" = "spotify";
+      workspace = "3 silent";
+    }
+    {
+      name = "thunderbird";
+      "match:class" = "org.mozilla.Thunderbird";
+      workspace = "4 silent";
+    }
+    {
+      name = "fractal";
+      "match:class" = "org.gnome.Fractal";
+      workspace = "4 silent";
+    }
   ]
   ++ lib.optionals isVoidFrame [
-    "workspace 4 silent,class:^(spotify)$"
+    {
+      name = "spotifyframe";
+      "match:class" = "spotify";
+      workspace = "4 silent";
+    }
   ];
 
-  windowrulev2 = [
-    # Battle.net
-    "float,title:^(Battle.net)$"
-    "workspace 11,title:^(Battle.net)$"
-    # World of Warcraft
-    "fullscreen,title:^(World of Warcraft)$"
-    "workspace 11,title:^(World of Warcraft)$"
-    # Thunderbird reminders
-    "suppressevent activatefocus,class:^(org.mozilla.Thunderbird)$,title:^(.*Reminder.*)$"
-    "float,class:^(org.mozilla.Thunderbird)$,title:^(.*Reminder.*)$"
-    "pin,class:^(org.mozilla.Thunderbird)$,title:^(.*Reminder.*)$"
-    "size 20% 30%,class:^(org.mozilla.Thunderbird)$,title:^(.*Reminder.*)$"
-    "move 100%-20%-10 100%-30%-10,class:^(org.mozilla.Thunderbird)$,title:^(.*Reminder.*)$"
-    # Quick kitty terminal
-    "float,class:^(kittyquick)$"
-    "pin,class:^(kittyquick)$"
-    # Firefox Picture-in-Picture
-    "suppressevent activatefocus,class:^(firefox)$,title:^(Picture-in-Picture)$"
-    "nofocus,class:^(firefox)$,title:^(Picture-in-Picture)$"
-    "float,class:^(firefox)$,title:^(Picture-in-Picture)$"
-    "pin,class:^(firefox)$,title:^(Picture-in-Picture)$"
-    "size 20% 30%,class:^(firefox)$,title:^(Picture-in-Picture)$"
-    "move 100%-20%-10 100%-30%-10,class:^(firefox)$,title:^(Picture-in-Picture)$"
+  layerrule = [
+    {
+      name = "noctaliahide";
+      "match:namespace" = "^noctalia-notifications.*$";
+      no_screen_share = "on";
+    }
   ];
-
-  layerrule = [ "noscreenshare,namespace:^(noctalia-notifications.*)$" ];
 }
