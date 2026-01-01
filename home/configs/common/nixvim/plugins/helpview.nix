d2927bff2b255a54c08ab85a62d6500517e0a76e
neonvoidx <me@neonvoid.dev>
2026-01-01 17:26:14 -0500

    wup

--- home/configs/common/nixvim/plugins/helpview.nix
+++ home/configs/common/nixvim/plugins/helpview.nix
@@ -1,5 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  # Plugin: OXY2DEV/helpview.nvim not available in nixvim
-  # This plugin provides enhanced help file viewing
+  # Plugin: OXY2DEV/helpview.nvim - Enhanced help file viewing
+  extraPlugins = [
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "helpview.nvim";
+      src = pkgs.fetchFromGitHub {
+        owner = "OXY2DEV";
+        repo = "helpview.nvim";
+        rev = "main";
+        sha256 = "sha256-Utcdm/9nAIfIx6oOXHgArS59/oTAOPg+wK+4/Z3TQUQ=";
+      };
+    })
+  ];
+
+  extraConfigLua = ''
+    require('helpview').setup({})
+  '';
 }
