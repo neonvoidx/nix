7195e6f5336ed0f99101c3e5a3bed2f9100374fb
neonvoidx <me@neonvoid.dev>
2026-01-01 16:28:29 -0500

    update hyprland scripts

--- home/configs/linux/hyprland/default.nix
+++ home/configs/linux/hyprland/default.nix
@@ -62,4 +62,10 @@ in
       };
     };
   };
+
+  # Hyprland configuration files
+  home.file.".config/hypr/scripts".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/assets/linux/hypr/scripts";
+  home.file.".config/hypr/hyprland/monitors".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/assets/linux/hypr/hyprland/monitors";
 }
--- home/neonvoid/files.nix
+++ home/neonvoid/files.nix
@@ -7,8 +7,6 @@
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/assets/linux/scripts";
   home.file.".config/scopebuddy".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/assets/linux/scopebuddy";
-  home.file.".config/hypr/scripts".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/assets/linux/hypr/scripts";
   home.file.".config/startupscripts".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/assets/linux/startupscripts";
 }
