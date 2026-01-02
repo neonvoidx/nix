c8c7b247785ced6872ada484a5a8ee186025582d
neonvoidx <me@neonvoid.dev>
2026-01-02 13:24:14 -0500

    fbset hook for boot resolution

--- hosts/voidframe/default.nix
+++ hosts/voidframe/default.nix
@@ -43,6 +43,18 @@
     };
   };
 
+  # Set framebuffer resolution during boot
+  systemd.services.fbset-resolution = {
+    description = "Set framebuffer resolution";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "systemd-vconsole-setup.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.fbset}/bin/fbset -xres 2880 -yres 1920";
+      RemainAfterExit = true;
+    };
+  };
+
   # Prevent rfkill from softblocking bluetooth and wifi
   systemd.services.rfkill-unblock = {
     description = "Unblock rfkill devices";
