16ce8d6b81222e99e44ced78213cfb276eb2ea88
neonvoidx <me@neonvoid.dev>
2026-01-01 20:42:56 -0500

    Added xdph and highlight-colors plugin

--- home/configs/common/nixvim/plugins/highlight-colors.nix
+++ home/configs/common/nixvim/plugins/highlight-colors.nix
@@ -2,9 +2,25 @@
 {
   plugins.nvim-colorizer = {
     enable = true;
-    userDefaultOptions = {
-      mode = "virtualtext";
-      virtualtext = "■";
+    settings = {
+      user_commands = true;
+      user_default_options = {
+        mode = "virtualtext";
+        names = false;
+        virtualtext = "■ ";
+        RGB = true;
+        RGBA = true;
+        RRGGBB = true;
+        RRGGBBAA = true;
+        AARRGGBB = true;
+        rgb_fn = true;
+        hsl_fn = true;
+        oklch_fn = true;
+        css = true;
+        css_fn = true;
+        tailwind = true;
+        xterm = true;
+      };
     };
   };
 }
