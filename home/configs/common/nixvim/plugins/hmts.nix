c2b37849418830db6745fd7b73a047885dd839d8
neonvoidx <me@neonvoid.dev>
2026-01-01 21:32:51 -0500

    nixvim plugins

--- home/configs/common/nixvim/plugins/hmts.nix
+++ home/configs/common/nixvim/plugins/hmts.nix
@@ -1,5 +1,6 @@
 { ... }:
 {
-  # Plugin: calops/hmts.nvim not available in nixvim
-  # This plugin provides Nix language support with tree-sitter
+  plugins.hmts = {
+    enable = true;
+  };
 }
--- home/configs/common/nixvim/plugins/quickfix.nix
+++ home/configs/common/nixvim/plugins/quickfix.nix
@@ -1,7 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  # Plugin: stevearc/quicker.nvim - enhanced quickfix UI
-  # Not available as a built-in nixvim plugin
-  # This plugin provides improved quickfix list with context lines, editable buffers, and better styling
-  # To add manually, use extraPlugins in nixvim configuration
+  extraPlugins = [
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "quicker.nvim";
+      src = pkgs.fetchFromGitHub {
+        owner = "stevearc";
+        repo = "quicker.nvim";
+        rev = "771437c3e3672dba9233156e1c2e2fc1888a5fff";
+        hash = "sha256-ce4UxGfM09hh5Z0LN0f9oFTrp0r5TzEi7dkxfuXZmf0=";
+      };
+    })
+  ];
 }
