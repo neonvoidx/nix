76420554e88c58690d53a04b48060e9c46b692f4
neonvoidx <me@neonvoid.dev>
2026-01-03 19:16:58 -0500

    fix for gtk theme up

--- home/configs/linux/gtk.nix
+++ home/configs/linux/gtk.nix
@@ -7,13 +7,15 @@
 }:
 {
   # Force Home Manager to overwrite existing GTK files
+  xdg.configFile."gtk-3.0/settings.ini".force = true;
+  xdg.configFile."gtk-4.0/settings.ini".force = true;
   xdg.configFile."gtk-4.0/gtk.css".force = true;
 
   gtk = {
     colorScheme = "dark";
     enable = true;
     theme = {
-      name = "nix-${config.colorScheme.slug}";
+      name = config.colorScheme.slug;
       package = (nix-colors.lib-contrib { inherit pkgs; }).gtkThemeFromScheme {
         scheme = config.colorScheme;
       };
