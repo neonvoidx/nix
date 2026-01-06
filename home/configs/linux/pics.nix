e6f283f95227f54672d554513af57eb7e11cc1d8
neonvoidx <me@neonvoid.dev>
2026-01-05 22:28:30 -0500

    attempt to quicken boot
    fixes boot time
    
    up
    
    up

--- home/configs/linux/pics.nix
+++ home/configs/linux/pics.nix
@@ -4,8 +4,6 @@ in {
   home.activation.clonePicsConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     if [ ! -d "${picsDir}" ]; then
       $DRY_RUN_CMD ${pkgs.git}/bin/git clone https://github.com/neonvoidx/pics "${picsDir}"
-    else
-      $DRY_RUN_CMD ${pkgs.git}/bin/git -C "${picsDir}" pull --ff-only || true
     fi
   '';
 }
