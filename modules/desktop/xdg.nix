cfb409c73b0a9c6098ae70380ea70a0d8e47d5cb
neonvoidx <me@neonvoid.dev>
2026-01-05 21:52:20 -0500

    wip

--- modules/desktop/xdg.nix
+++ modules/desktop/xdg.nix
@@ -1,11 +1,18 @@
 { pkgs, ... }:
 {
-  xdg.portal = {
-    enable = true;
-    xdgOpenUsePortal = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal
-      xdg-desktop-portal-gtk
-    ];
+  xdg = {
+    portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal
+        xdg-desktop-portal-gtk
+      ];
+    };
+    # mime.enable = true;
+    # mimeApps = {
+    #   enable = true;
+    #   defaultApplications = { };
+    # };
   };
 }
