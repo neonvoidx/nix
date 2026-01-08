6100cfc6ea66e18472f2db14dd6617c2148af7f5
neonvoidx <me@neonvoid.dev>
2026-01-08 11:57:17 -0500

    moved pipewire stuff to void only

--- hosts/void/pipewire.nix
+++ hosts/void/pipewire.nix
@@ -1,5 +1,37 @@
 { ... }:
 {
+
+  services.pipewire = {
+    extraConfig.pipewire = {
+      "10-low-latency.conf" = {
+        "context.properties" = {
+          "default.frags" = 8;
+          "default.frag-size" = 4096;
+        };
+      };
+      "10-max-buffers" = {
+        "context.properties" = {
+          "link.max-buffers" = 64;
+        };
+      };
+      "11-clock-rates" = {
+        "context-properties" = {
+          "default.clock.rate" = 96000;
+          "default.clock.allowed-rates" = [
+            44100
+            48000
+            88200
+            96000
+            176400
+            192000
+          ];
+          "default.clock.quantum" = 4096;
+          "default.clock.min-quantum" = 1025;
+          "default.clock.max-quantum" = 4096;
+        };
+      };
+    };
+  };
   services.pipewire.wireplumber.extraConfig = {
     "51-disable-devices" = {
       "monitor.alsa.rules" = [
