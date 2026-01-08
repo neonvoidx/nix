60c5061807c83200a179e5862d41a0691e199cf7
neonvoidx <me@neonvoid.dev>
2026-01-08 17:57:29 -0500

    update console colors

--- modules/system/locale.nix
+++ modules/system/locale.nix
@@ -7,24 +7,25 @@
     font = "${pkgs.terminus_font}/share/consolefonts/ter-118b.psf.gz";
     packages = with pkgs; [ terminus_font ];
     keyMap = "us";
-    colors = [
-      "21222c"
-      "7081d0"
-      "f9515d"
-      "f16c75"
-      "37f499"
-      "69F8B3"
-      "e9f941"
-      "f1fc79"
-      "9071f4"
-      "a48cf2"
-      "f265b5"
-      "FD92CE"
-      "04d1f9"
-      "66e4fd"
-      "ebfafa"
-      "ffffff"
-    ];
+    # Using Stylix, leaving here for future if needed
+    # colors = [
+    #   "21222c"
+    #   "7081d0"
+    #   "f9515d"
+    #   "f16c75"
+    #   "37f499"
+    #   "69F8B3"
+    #   "e9f941"
+    #   "f1fc79"
+    #   "9071f4"
+    #   "a48cf2"
+    #   "f265b5"
+    #   "FD92CE"
+    #   "04d1f9"
+    #   "66e4fd"
+    #   "ebfafa"
+    #   "ffffff"
+    # ];
   };
 
   time.hardwareClockInLocalTime = true;
