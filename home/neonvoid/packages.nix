2abd10e5cc1ac2af89cb6d55fcb977ba5a92d3ee
neonvoidx <me@neonvoid.dev>
2026-01-17 00:06:49 -0500

    gpu-screen-recorder

--- home/neonvoid/packages.nix
+++ home/neonvoid/packages.nix
@@ -6,44 +6,54 @@
   ...
 }:
 {
-  home.packages = with pkgs; [
-    (pkgs.callPackage ../../modules/programs/scopebuddy.nix { })
-    (pkgs.callPackage ../../modules/programs/hyprshutdown.nix { })
-    aseprite
-    asciinema
-    blueman
-    cmatrix
-    gamescope
-    gimp
-    github-copilot-cli
-    grim
-    gvfs
-    hyprpicker
-    hyprsysteminfo
-    file-roller
-    kdePackages.okular
-    libsecret
-    nix-versions.packages.${pkgs.stdenv.hostPlatform.system}.default
-    oculante
-    pinta
-    protontricks
-    protonup-rs
-    prusa-slicer
-    hyprpwcenter
-    seahorse
-    sgdboop
-    slurp
-    steam
-    streamcontroller
-    tenacity
-    vulkan-tools
-    wl-clipboard
-    thunar
-    thunar-archive-plugin
-    thunar-media-tags-plugin
-    thunar-volman
-  ] ++ (if hostname == "void" then [
-    deadlock-mod-manager
-    inputs.hytale-launcher.packages.${pkgs.stdenv.hostPlatform.system}.default
-  ] else []);
+  home.packages =
+    with pkgs;
+    [
+      (pkgs.callPackage ../../modules/programs/scopebuddy.nix { })
+      (pkgs.callPackage ../../modules/programs/hyprshutdown.nix { })
+      aseprite
+      asciinema
+      blueman
+      cmatrix
+      gamescope
+      gimp
+      github-copilot-cli
+      gpu-screen-recorder
+      gpu-screen-recorder-gtk
+      grim
+      gvfs
+      hyprpicker
+      hyprsysteminfo
+      file-roller
+      kdePackages.okular
+      libsecret
+      nix-versions.packages.${pkgs.stdenv.hostPlatform.system}.default
+      oculante
+      pinta
+      protontricks
+      protonup-rs
+      prusa-slicer
+      hyprpwcenter
+      seahorse
+      sgdboop
+      slurp
+      steam
+      streamcontroller
+      tenacity
+      vulkan-tools
+      wl-clipboard
+      thunar
+      thunar-archive-plugin
+      thunar-media-tags-plugin
+      thunar-volman
+    ]
+    ++ (
+      if hostname == "void" then
+        [
+          deadlock-mod-manager
+          inputs.hytale-launcher.packages.${pkgs.stdenv.hostPlatform.system}.default
+        ]
+      else
+        [ ]
+    );
 }
