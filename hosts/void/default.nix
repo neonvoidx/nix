3cb80e9a8d81edd30d5e3f764cf5bb253ebb6cb4
neonvoidx <me@neonvoid.dev>
2026-01-18 19:23:57 -0500

    back to wowup, fix substituters

--- hosts/void/default.nix
+++ hosts/void/default.nix
@@ -134,6 +134,7 @@
   environment.systemPackages = with pkgs; [
     sbctl
     amdgpu_top
+    wowup-cf
     (blender.override { rocmSupport = true; }) # adds hardware acceleration for AMD to Blender
   ];
 
