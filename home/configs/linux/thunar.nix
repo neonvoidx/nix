199134200926db0b5da6123c7d56ca3c8bbd8ece
neonvoidx <me@neonvoid.dev>
2026-01-21 09:53:47 -0500

    update thunar context

--- home/configs/linux/thunar.nix
+++ home/configs/linux/thunar.nix
@@ -1,8 +1,37 @@
-{ ... }:
+{ pkgs, ... }:
 {
   dconf.settings = {
     "org/gtk/settings/file-chooser" = {
       show-hidden = true;
     };
   };
+
+  xfconf.settings = {
+    xfce4-terminal = {
+      "terminal/emulator" = "kitty";
+    };
+  };
+
+  xdg.configFile."Thunar/uca.xml".text = ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <actions>
+      <action>
+        <icon>utilities-terminal</icon>
+        <name>Open Terminal Here</name>
+        <command>kitty --working-directory %f</command>
+        <description>Open kitty terminal in the current directory</description>
+        <patterns>*</patterns>
+        <startup-notify/>
+        <directories/>
+      </action>
+    </actions>
+  '';
+
+  home.packages = with pkgs; [
+    xfce.exo
+  ];
+
+  xdg.configFile."xfce4/helpers.rc".text = ''
+    TerminalEmulator=kitty
+  '';
 }
