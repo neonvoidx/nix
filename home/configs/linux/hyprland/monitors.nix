c9e19ff9e38ff59631b60c44e1b6a259020bbacb
neonvoidx <me@neonvoid.dev>
2026-01-27 09:04:41 -0500

    nohdr configs for hypr monitors nohdr nohdr

--- home/configs/linux/hyprland/monitors.nix
+++ home/configs/linux/hyprland/monitors.nix
@@ -12,8 +12,8 @@ in
     ",preferred,auto,1"
   ]
   ++ lib.optionals isVoid [
-    "DP-2,3440x1440@143.92,4880x1440,1.0,bitdepth,10,cm,hdredid,sdrbrightness,1.3,sdrsaturation,0.93,vrr,1"
-    "DP-3,3440x1440@143.92,4880x0,1.0,bitdepth,10,cm,hdredid,sdrbrightness,1.3,sdrsaturation,0.93,vrr,1"
+    "DP-2,3440x1440@143.92,4880x1440,1.0"
+    "DP-3,3440x1440@143.92,4880x0,1.0"
     "HDMI-A-1,2560x1440@59.95,3440x727,1.0,transform,1"
   ]
   ++ lib.optionals isVoidFrame [
