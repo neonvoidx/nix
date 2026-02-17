bf2e3ebea891e7487de5cb1671e330abb66498bf
neonvoidx <me@neonvoid.dev>
2026-02-17 14:49:34 -0500

    fbset greetd up up

--- modules/services/greetd.nix
+++ modules/services/greetd.nix
@@ -1,7 +1,12 @@
 { ... }:
 {
   flake.modules.nixos.greetd =
-    { pkgs, ... }:
+    {
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
     {
       services.greetd = {
         enable = true;
@@ -11,6 +16,12 @@
             user = "greeter";
           };
         };
+      };
+
+      systemd.services.greetd = lib.mkIf (config.networking.hostName == "void") {
+        preStart = ''
+          ${pkgs.fbset}/bin/fbset -xres 3440 -yres 1440
+        '';
       };
     };
 }
