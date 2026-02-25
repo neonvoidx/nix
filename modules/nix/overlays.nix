2e7c4c96a5582394824086bb9d6112d63fcdddcd
neonvoidx <me@neonvoid.dev>
2026-02-24 19:54:09 -0500

    temp fix for picosvg
    up

--- modules/nix/overlays.nix
+++ modules/nix/overlays.nix
@@ -10,6 +10,21 @@
             pkgs = prev;
           };
         })
+        # TODO: remove once https://github.com/nixos/nixpkgs/pull/493376 lands in nixos-unstable
+        (final: prev: {
+          pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+            (_: pyPrev: {
+              picosvg = pyPrev.picosvg.overridePythonAttrs (old: {
+                patches = (old.patches or [ ]) ++ [
+                  (final.fetchpatch {
+                    url = "https://github.com/googlefonts/picosvg/commit/885ee64b75f526e938eb76e09fab7d93e946a355.patch";
+                    hash = "sha256-fR3FfnEPHwSO1rMtmQEr1pyvByTx8T53FxSpuAKWIjw=";
+                  })
+                ];
+              });
+            })
+          ];
+        })
       ];
     };
 }
