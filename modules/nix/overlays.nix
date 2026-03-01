6af220b4fedbcd21646dd5549ae0af291e223fad
neonvoidx <me@neonvoid.dev>
2026-02-28 22:35:45 -0500

    picosvg merged

--- modules/nix/overlays.nix
+++ modules/nix/overlays.nix
@@ -10,21 +10,6 @@
             pkgs = prev;
           };
         })
-        # WAITING: https://nixpkgs-tracker.ocfox.me/?pr=493376
-        (final: prev: {
-          pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-            (_: pyPrev: {
-              picosvg = pyPrev.picosvg.overridePythonAttrs (old: {
-                patches = (old.patches or [ ]) ++ [
-                  (final.fetchpatch {
-                    url = "https://github.com/googlefonts/picosvg/commit/885ee64b75f526e938eb76e09fab7d93e946a355.patch";
-                    hash = "sha256-fR3FfnEPHwSO1rMtmQEr1pyvByTx8T53FxSpuAKWIjw=";
-                  })
-                ];
-              });
-            })
-          ];
-        })
         # WAITING: remove once https://github.com/NixOS/nixpkgs/pull/493565 lands in nixos-unstable
         (final: prev: {
           deadlock-mod-manager = prev.deadlock-mod-manager.overrideAttrs (old: rec {
