292c0ecd895386043115a99d36aa95319df4f695
neonvoidx <me@neonvoid.dev>
2026-04-29 17:58:40 -0400

    cage for regreet over hyprland

--- modules/security/regreet.nix
+++ modules/security/regreet.nix
@@ -4,57 +4,17 @@
     { host, ... }:
     {
       nixos =
+        { ... }:
         {
-          pkgs,
-          lib,
-          ...
-        }:
-        {
-          services.greetd = {
-            enable = true;
-            settings.default_session = {
-              command = "${pkgs.hyprland}/bin/start-hyprland --config /etc/greetd/hyprland.conf";
-              user = "greeter";
-            };
-          };
+          services.greetd.enable = true;
 
           programs.regreet = {
             enable = true;
             settings = {
-              appearance = {
-                greeting_msg = host.greeting or "Welcome back!";
-              };
+              appearance.greeting_msg = host.greeting or "Welcome back!";
+              GTK.application_prefer_dark_theme = true;
             };
           };
-
-          # Minimal Hyprland config for the greeter session.
-          # Launches regreet; exits Hyprland once regreet closes (user logged in).
-          environment.etc."greetd/hyprland.conf".text = ''
-            misc {
-              disable_hyprland_logo = true
-              disable_splash_rendering = true
-              force_default_wallpaper = 0
-            }
-
-            animations {
-              enabled = false
-            }
-
-            ${lib.optionalString (host.isMultiMonitor or false) ''
-              monitor=DP-2,3440x1440@143.92,4880x1440,1.0,bitdepth,10, cm, hdr, sdrbrightness, 1.3, sdrsaturation, 0.93, vrr, 1
-              monitor=DP-3,3440x1440@143.92,4880x0,1.0,bitdepth,10, cm, hdr, sdrbrightness, 1.3, sdrsaturation, 0.93, vrr, 1
-              monitor=HDMI-A-1,2560x1440@59.95,3440x727,1.0
-              monitor=HDMI-A-1,transform,1
-            ''}
-            ${lib.optionalString (host.isLaptop or false) ''
-              monitor = eDP-1,2880x1920@120,0x0,1.33333
-            ''}
-            monitor = ,preferred,auto,1
-
-            windowrule = match:class regreet, fullscreen on
-
-            exec-once = ${lib.getExe pkgs.regreet} && hyprctl dispatch exit 0
-          '';
 
           # Unlock gnome-keyring via greetd's PAM session
           security.pam.services.greetd.enableGnomeKeyring = true;
