1173e6081bf8c5b116d7bbbf22e764ef77ff7984
neonvoidx <me@neonvoid.dev>
2026-05-12 16:06:53 -0400

    update with cachix key
    update

--- modules/security/sops.nix
+++ modules/security/sops.nix
@@ -9,9 +9,16 @@
           imports = [ inputs.sops-nix.nixosModules.sops ];
           sops = {
             defaultSopsFile = inputs.self + "/secrets/secrets.yaml";
-            age.sshKeyPaths = builtins.map (user: "/home/${user.userName}/.ssh/id_ed25519") (builtins.attrValues host.users);
+            age.sshKeyPaths = builtins.map (user: "/home/${user.userName}/.ssh/id_ed25519") (
+              builtins.attrValues host.users
+            );
             validateSopsFiles = false;
-            secrets = { };
+            secrets = {
+              "cachix" = {
+                owner = "neonvoid";
+                mode = "0400";
+              };
+            };
           };
         };
     };
