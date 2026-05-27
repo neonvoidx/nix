c1cdc0185d46d93c1177abb26acdfc92420c47e1
neonvoidx <me@neonvoid.dev>
2026-05-26 23:15:44 -0400

    moved neovim to shell , fix mcp

--- modules/shell/mcp.nix
+++ modules/shell/mcp.nix
@@ -1,33 +1,27 @@
 { den, ... }:
 {
-  den.aspects.mcp =
+  den.aspects.mcp.homeManager =
     { ... }:
     {
-      homeManager =
-        {
-          ...
-        }:
-        {
-          programs.mcp = {
-            enable = true;
-            servers = {
-              mcp-nixos = {
-                command = "nix";
-                args = [
-                  "run"
-                  "github:utensils/mcp-nixos"
-                  "--"
-                ];
-              };
-              nix-agent = {
-                command = "nix";
-                args = [
-                  "run"
-                  "github:JEFF7712/nix-agent"
-                ];
-              };
-            };
+      programs.mcp = {
+        enable = true;
+        servers = {
+          mcp-nixos = {
+            command = "nix";
+            args = [
+              "run"
+              "github:utensils/mcp-nixos"
+              "--"
+            ];
+          };
+          nix-agent = {
+            command = "nix";
+            args = [
+              "run"
+              "github:JEFF7712/nix-agent"
+            ];
           };
         };
+      };
     };
 }
