198f1fbaa0902f3763b89cb65a3d4b8af546f1ae
neonvoidx <me@neonvoid.dev>
2026-05-27 14:52:09 -0400

    nh updates

--- modules/shell/nh.nix
+++ modules/shell/nh.nix
@@ -5,11 +5,6 @@
     {
       programs.nh = {
         enable = true;
-        clean = {
-          enable = true;
-          dates = "weekly";
-          extraArgs = "-k 5";
-        };
         flake = "$HOME/nix";
         homeFlake = "$HOME/nix";
         osFlake = "$HOME/nix";
