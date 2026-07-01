c9e1bddac2210b816b89bca300c08d29e81dd629
neonvoidx <me@neonvoid.dev>
2026-07-01 15:56:38 -0400

    regreet
    up
    
    regreet

--- modules/security/regreet.nix
+++ modules/security/regreet.nix
@@ -1,10 +1,28 @@
-{ den, ... }:
+{ den, lib, ... }:
 {
   den.aspects.regreet =
     { host, ... }:
     {
       nixos =
-        { ... }:
+        { pkgs, config, ... }:
+        let
+          mkGreeterConfig = regreetPkg:
+            pkgs.writeText "greeter-hyprland.lua" ''
+              ${host.greeterMonitorConfig or "-- no monitor config"}
+
+              hl.on("hyprland.start", function()
+                hl.exec_cmd("${lib.getExe regreetPkg}; hyprctl dispatch exit")
+              end)
+
+              hl.config({
+                misc = {
+                  disable_hyprland_logo = true,
+                  disable_splash_rendering = true,
+                  disable_hyprland_guiutils_check = true,
+                },
+              })
+            '';
+        in
         {
           services.greetd.enable = true;
 
@@ -16,7 +34,9 @@
             };
           };
 
-          # Unlock gnome-keyring via greetd's PAM session
+          services.greetd.settings.default_session.command =
+            "${pkgs.dbus}/bin/dbus-run-session ${pkgs.hyprland}/bin/start-hyprland -- -c ${mkGreeterConfig config.programs.regreet.package}";
+
           security.pam.services.greetd.enableGnomeKeyring = true;
         };
     };
