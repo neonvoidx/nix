3634be5f851e4e40910913d38b669891e9ccfba4
neonvoidx <me@neonvoid.dev>
2026-07-29 14:49:06 -0400

    added curseforge removed wowup

--- modules/gaming/wow.nix
+++ modules/gaming/wow.nix
@@ -2,15 +2,40 @@
 {
   den.aspects.wow.homeManager =
     { pkgs, ... }:
+    let
+      curseforge = pkgs.fetchurl {
+        url = "https://curseforge.overwolf.com/downloads/curseforge-latest-linux.AppImage";
+        hash = "sha256-ocLq6EaM2E/yvH9zH2ESXZ8eLiquRHxStT/CDhJ2OdQ=";
+      };
+    in
     {
-      home.packages = with pkgs; [
-        wowup-cf
+      home.packages = [
+        (pkgs.appimageTools.wrapType2 {
+          pname = "CurseForge";
+          name = "curseforge";
+          version = "1.314.0";
+          src = curseforge;
+          extraInstallCommands =
+            let
+              contents = pkgs.appimageTools.extract {
+                pname = "CurseForge";
+                version = "1.314.0";
+                src = curseforge;
+              };
+            in
+            ''
+              install -m 444 -D ${contents}/curseforge.desktop $out/share/applications/curseforge.desktop
+              substituteInPlace $out/share/applications/curseforge.desktop \
+                  --replace-fail 'Exec=AppRun' 'Exec=CurseForge'
+              cp -r ${contents}/usr/share/icons $out/share/icons
+            '';
+        })
       ];
 
       xdg.mimeApps = {
         enable = true;
         defaultApplications = {
-          "x-scheme-handler/curseforge" = "wowup-cf.desktop";
+          "x-scheme-handler/curseforge" = "curseforge.desktop";
         };
       };
     };
