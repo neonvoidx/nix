69353ebf551291c244c1c3baf47ae6776e05e8dd
neonvoidx <me@neonvoid.dev>
2026-08-11 13:44:48 -0400

    blender fix

--- modules/hosts/void/default.nix
+++ modules/hosts/void/default.nix
@@ -112,7 +112,7 @@
         environment.systemPackages = with pkgs; [
           sbctl
           amdgpu_top
-          blender-rocm
+          pkgsRocm.blender
         ];
 
         networking = {
