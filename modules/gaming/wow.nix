40321b34d58f5daa66c54f926f29dc44e5caf9ba
neonvoidx <me@neonvoid.dev>
2026-08-26 08:54:59 -0400

    add wc logs uploader

--- modules/gaming/wow.nix
+++ modules/gaming/wow.nix
@@ -7,6 +7,10 @@
         url = "https://curseforge.overwolf.com/downloads/curseforge-latest-linux.AppImage";
         hash = "sha256-ocLq6EaM2E/yvH9zH2ESXZ8eLiquRHxStT/CDhJ2OdQ=";
       };
+      archon-lite = pkgs.fetchurl {
+        url = "https://github.com/RPGLogs/Uploaders-archon-lite/releases/download/v9.5.0/archon-lite-v9.5.0.AppImage";
+        hash = "sha256-ZuALgVtqsYtvnSq8hkJL4A+i4UaEpk+2L3bpSEXrhRM=";
+      };
     in
     {
       home.packages = [
@@ -28,6 +32,26 @@
               substituteInPlace $out/share/applications/curseforge.desktop \
                   --replace-fail 'Exec=AppRun' 'Exec=CurseForge'
               cp -r ${contents}/usr/share/icons $out/share/icons
+            '';
+        })
+        (pkgs.appimageTools.wrapType2 {
+          pname = "archon-lite";
+          name = "archon-lite";
+          version = "9.5.0";
+          src = archon-lite;
+          extraInstallCommands =
+            let
+              contents = pkgs.appimageTools.extract {
+                pname = "archon-lite";
+                version = "9.5.0";
+                src = archon-lite;
+              };
+            in
+            ''
+              install -m 444 -D ${contents}/archon-lite.desktop $out/share/applications/archon-lite.desktop
+              substituteInPlace $out/share/applications/archon-lite.desktop \
+                  --replace-fail 'Exec=AppRun' 'Exec=archon-lite'
+              install -m 444 -D ${contents}/archon-lite.png $out/share/icons/hicolor/256x256/apps/archon-lite.png
             '';
         })
         pkgs.xembsni
