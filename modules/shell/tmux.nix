2d0af37504f57c3bb1b5c76e1d3d59379bb57403
neonvoidx <me@neonvoid.dev>
2026-09-09 15:39:44 -0400

    tmux fix

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -56,8 +56,9 @@
             set -g renumber-windows on
             setw -g pane-base-index 1
 
-            # Allows passing of keys like ctrl+enter shift+enter etc
-            set -g extend-keys on
+            # Pass modified keys (Shift+Enter, Ctrl+Enter, etc.) to programs inside tmux
+            set -g extended-keys on
+            set -as terminal-features 'xterm*:extkeys'
 
             # Pane border
             set -g pane-border-style fg=#${c.base02}
