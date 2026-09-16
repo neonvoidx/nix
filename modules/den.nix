d39e4039e9169483902d2262eee53745ed244f96
neonvoidx <me@neonvoid.dev>
2026-09-16 14:36:25 -0400

    fix for denful moving org repos

--- modules/den.nix
+++ modules/den.nix
@@ -6,8 +6,8 @@
 }:
 {
   # Setup for den
-  flake-file.inputs.flake-file.url = lib.mkDefault "github:vic/flake-file";
-  flake-file.inputs.den.url = lib.mkDefault "github:vic/den";
+  flake-file.inputs.flake-file.url = lib.mkDefault "github:denful/flake-file";
+  flake-file.inputs.den.url = lib.mkDefault "github:denful/den";
   imports = [
     (inputs.flake-file.flakeModules.dendritic or { })
     (inputs.den.flakeModules.dendritic or { })
