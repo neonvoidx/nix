6a0f282bde420eb2d4eade622b633ecd3424d46d
neonvoidx <me@neonvoid.dev>
2026-09-16 14:52:46 -0400

    tmux rename window fix

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -153,7 +153,7 @@
             bind t command-prompt -I '#S' 'rename-session -- "%%"'
             bind '$' command-prompt -I '#S' 'rename-session -- "%%"'
             # Manual rename (prefix+,) also disables auto-renaming so the name sticks.
-            bind ',' command-prompt -I '#W' 'rename-window -- "%%" \; set -w automatic-rename off'
+            bind , command-prompt -I '#W' "rename-window '%%'"
 
             # Window navigation (with prefix)
             bind C-h select-window -t :-
